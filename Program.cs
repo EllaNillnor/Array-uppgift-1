2ca00b57e8276cee89780698aac01eb58b5b0c9b
EllaNillnor <elni06001@utb.vaxjo.se>
2024-10-21 09:17:02 +0200

    array uppgift 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+int [] v= new int[5];
+
+for(int i = 0 ; i<5;i++){
+    Console.Write("skriv ett tal: ");
+    int svar = int.Parse(Console.ReadLine());
+    v[i]= svar; 
+}
+
+for (int i = 4; i>-1;i--){
+    Console.Write(v[i]+" ");
+}
